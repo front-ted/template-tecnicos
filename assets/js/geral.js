193cf2981eaaf04cc47aff12360be82638e7d168
brunaflores-gete <80692164+brunaflores-gete@users.noreply.github.com>
2023-06-20 09:01:35 -0300

    atualizações

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -4,6 +4,11 @@ window.addEventListener('load', function(){
       return new bootstrap.Tooltip(tooltipTriggerEl)
     })
 
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+
     $('.preloader').fadeOut(300);
 
 
