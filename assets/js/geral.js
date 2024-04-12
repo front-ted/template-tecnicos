2169ce39c792f67456bf990a11ffb6bb0be63a43
FabianaRossatto <fmrossatto@senacrs.com.br>
2024-04-12 10:09:36 -0300

    modo-escuro-e-sombras

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -25,60 +25,29 @@ $(window).scroll(function () {
 
   // if user scrolls down..
   if (topPos > 100) {
-    $('body').removeClass("mgTopAltoContraste");
-    $('body').addClass("mgTopAltoContraste_00");
+    $('body').removeClass("top60");
+    $('body').addClass("top00");
     $('.bt-hamburguer').css("top", "20px");
     $('.bt-fecharmenu').css("top", "20px");
   } else {
-    $('body').removeClass("mgTopAltoContraste_00");
-    $('body').addClass("mgTopAltoContraste");
+    $('body').removeClass("top00");
+    $('body').addClass("top60");
     $('.bt-hamburguer').css("top", "150px");      
     $('.bt-fecharmenu').css("top", "150px");      
   }
 
 }); // scroll END
 
-// alto contraste
 
-jQuery("document").ready(function($){
+var isDarkMode = true; // Controla o modo atual (escuro/claro)
 
-  //localStorage.clear();
+function toggleBackgroundImage() {
+    var button = document.getElementById('toggleButton');
+    isDarkMode = !isDarkMode; // Alternar entre escuro e claro
 
-  var tema_pagina;
-
-  $(".btn-tema_pagina").click(function(){
-
-    console.log($(".btn-tema_pagina"));
-
-    if($("body").hasClass('bg-claro')){
-      console.log("sim");
-      $("body").removeClass('bg-claro');
-      $("body").addClass("bg-escuro");
-      $('.btn-tema_pagina').css("background-image", "url(../images/mode_dark.png)");
-      localStorage.setItem('tema_pagina', 'bg-escuro');
-    }else if($("body").hasClass('bg-escuro')){
-      console.log("não");
-      $("body").removeClass('bg-escuro');
-      $("body").addClass("bg-claro");
-      $('.btn-tema_pagina').css("background-image", "url(../images/mode_light.png)");
-      localStorage.setItem('tema_pagina', 'bg-claro');
-    }else{
-      console.log("sim");
-      $("body").addClass("bg-escuro");
-      localStorage.setItem('tema_pagina', 'bg-escuro');
+    if (isDarkMode) {
+        button.style.backgroundImage = "url('../assets/images/modo_escuro.svg')";
+    } else {
+        button.style.backgroundImage = "url('../assets/images/modo_claro.svg')";
     }
-
-  });
-
-  tema_pagina = localStorage.getItem('tema_pagina');
-
-  if (tema_pagina == 'bg-claro'){
-    $("body").addClass("bg-claro");
-  }
-  if (tema_pagina == 'bg-escuro'){
-    $("body").addClass("bg-escuro");
-  }else{
-    $("body").addClass("bg-claro");
-  }
-
-});
+}
